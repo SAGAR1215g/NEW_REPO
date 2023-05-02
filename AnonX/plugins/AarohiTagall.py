0c93c91e548f72362eedb13b6bd515d428fdc23e
˹ᴋʀɪsʜɴᴀ ˼ <119519282+RocksKrishnaBot@users.noreply.github.com>
2023-05-02 16:19:38 +0530

    Update AarohiTagall.py

--- AnonX/plugins/AarohiTagall.py
+++ AnonX/plugins/AarohiTagall.py
@@ -5,7 +5,6 @@ from pyrogram import Client, filters
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
 
-
 spam_chats = []
 
 EMOJI = ["🦋🦋🦋🦋🦋"]
@@ -42,4 +41,50 @@ async def mentionall(client, message):
         if not msg:
             return await message.reply("/tagall hii 👈 Try this or reply any message...")
     else:
-        return await message.reply("/tagall hii 👈 Try this or 
+        return await message.reply("/tagall hii 👈 Try this or reply any message...")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ""
+    async for usr in client.iter_chat_members(chat_id):
+        if not chat_id in spam_chats:
+            break
+        if usr.user.is_bot:
+            continue
+        usrnum += 1
+        usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}) "
+
+        if usrnum == 1:
+            if mode == "text_on_cmd":
+                txt = f"{usrtxt} {random.choice(TAGMES)}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})")
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ""
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
+
+@app.on_message(filters.command(["cancel", "stop"]))
+async def cancel_spam(client, message):
+    if not message.chat.id in spam_chats:
+        return await message.reply("No active mention process is started by me.")
+    is_admin = False
+    try:
+        participant = await client.get_chat_member(message.chat.id, message.from_user.id)
+    except UserNotParticipant:
+        is_admin = False
+    else:
+        if participant.status in ("administrator", "creator"):
+            is_admin = True
+    if not is_admin:
+        return await message.reply("This command is only for admins. You can't use this command.")
+    else:
+        try:
+            spam_chats.remove(message.chat.id)
+        except:
+            pass
+        return await message.reply("♦ Mention process stopped ♦")
