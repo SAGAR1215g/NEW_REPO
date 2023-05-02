fc17f0a3fdbb242115fe83b6d3108431cb48cc09
bnznsnsnsksk <126651350+SAGAR1215g@users.noreply.github.com>
2023-05-02 16:11:23 +0530

    Create AarohiTagall.py

---
+++ AnonX/plugins/AarohiTagall.py
@@ -0,0 +1,45 @@
+from AnonX import app 
+import asyncio
+import random
+from pyrogram import Client, filters
+from pyrogram.errors import UserNotParticipant
+from pyrogram.types import ChatPermissions
+
+
+spam_chats = []
+
+EMOJI = ["🦋🦋🦋🦋🦋"]
+
+TAGMES = [" 𝐇𝐞𝐲 𝐁𝐚𝐛𝐲 𝐊𝐚𝐡𝐚 𝐇𝐨🥱 ",
+          " 𝐆𝐨𝐨𝐝 𝐍8 𝐉𝐢 𝐁𝐡𝐮𝐭 𝐑𝐚𝐭 𝐇𝐨 𝐠𝐲𝐢🥰 ",
+          ]
+
+@app.on_message(filters.command(["tagall", "all", "tagmember"], prefixes=["/", "@", "#"]))
+async def mentionall(client, message):
+    chat_id = message.chat.id
+    if message.chat.type == "private":
+        return await message.reply("This command can be used in groups and channels!")
+
+    is_admin = False
+    try:
+        participant = await client.get_chat_member(chat_id, message.from_user.id)
+    except UserNotParticipant:
+        is_admin = False
+    else:
+        if participant.status in ("administrator", "creator"):
+            is_admin = True
+    if not is_admin:
+        return await message.reply("Only admin can use this command!")
+
+    if message.reply_to_message and message.text:
+        return await message.reply("/tagall hello 👈 Try this next time for tagging..")
+    elif message.text:
+        mode = "text_on_cmd"
+        msg = message.text
+    elif message.reply_to_message:
+        mode = "text_on_reply"
+        msg = message.reply_to_message
+        if not msg:
+            return await message.reply("/tagall hii 👈 Try this or reply any message...")
+    else:
+        return await message.reply("/tagall hii 👈 Try this or 
